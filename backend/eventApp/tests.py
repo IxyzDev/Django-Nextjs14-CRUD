9092ac9256e7db6d6ef380b6a19ddce52f081acb
Ixyz.dev <kevin.diaz@alumnos.uv.cl>
2023-12-04 20:00:18 -0300

    Implementado 5 pruebas mas

--- backend/eventApp/tests.py
+++ backend/eventApp/tests.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Event
 
+import time
+
 # Ejecutar los Test: python manage.py test eventApp
 
 
@@ -52,3 +54,50 @@ class EventAPITestCase(APITestCase):
         response = self.client.delete(
             reverse('events-detail', kwargs={'pk': event.id}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_create_event_with_missing_optional_fields(self):
+        """
+        Prueba la creación de un evento sin algunos campos opcionales.
+        """
+        data = {"title": "Event Without Optional Fields",
+                "date": "2021-01-03T00:00:00Z"}
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # Puedes agregar más aserciones aquí para verificar los campos omitidos
+
+    def test_create_event_with_large_data(self):
+        """
+        Prueba la creación de un evento con datos que exceden los límites de tamaño.
+        """
+        large_title = 'a' * 300  # Asumiendo que el límite es menor
+        data = {"title": large_title, "description": "Large Data Test",
+                "date": "2021-01-03T00:00:00Z", "location": "Large Location"}
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_event_with_invalid_date_format(self):
+        """
+        Prueba la creación de un evento con un formato de fecha inválido.
+        """
+        data = {"title": "Invalid Date Format", "description": "Invalid Date",
+                "date": "2021-01-32T00:00:00Z", "location": "Invalid Location"}
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nonexistent_endpoint(self):
+        """
+        Prueba el acceso a un endpoint que no existe.
+        """
+        response = self.client.get('/nonexistent-endpoint/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_response_time(self):
+        """
+        Prueba el tiempo de respuesta para una solicitud específica.
+        """
+        start_time = time.time()
+        response = self.client.get(reverse('events-list'))
+        end_time = time.time()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # por ejemplo, menos de 0.5 segundos
+        self.assertTrue(end_time - start_time < 0.5)
